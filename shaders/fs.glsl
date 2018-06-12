444d8cf5dd563b50e722469486ea03ee61fbb99d
Romeo Zeph <r.s.b.zeph@students.uu.nl>
2018-06-12 11:57:35 +0200

    color weg

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -12,5 +12,5 @@ out vec4 outputColor;
 // fragment shader
 void main()
 {
-    outputColor = texture( pixels, uv ) + 0.5f * vec4( normal.xyz, 1 );
+    outputColor = texture( pixels, uv ) + 0.5f;
 }
