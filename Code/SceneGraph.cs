176095d9a039af0004664fc139034479fb9af82c
Robert van den Brink <robertvdenbrink@gmail.com>
2018-06-12 12:41:32 +0200

    Node

--- Code/SceneGraph.cs
+++ Code/SceneGraph.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Template_P3;
 
 class SceneGraph
 {
+    public List<Mesh> children = new List<Mesh>();
+
     public void Render()
     { 
 
