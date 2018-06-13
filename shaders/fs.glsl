bd255e09b6ea8b20efcb0da5562efa0a5f32e4bb
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-06-13 10:30:33 +0200

    ss

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -3,14 +3,15 @@
 // shader input
 in vec2 uv;						// interpolated texture coordinates
 in vec4 normal;					// interpolated normal
+in vec4 worldPos;
+
 uniform sampler2D pixels;		// texture sampler
 
 // shader output
 out vec4 outputColor;
 
-
 // fragment shader
 void main()
 {
-    outputColor = texture( pixels, uv ) + 0.5f * vec4( normal.xyz, 1 );
+    outputColor = texture( pixels, uv ); // + 0.5f * vec4( normal.xyz, 1 );
 }
