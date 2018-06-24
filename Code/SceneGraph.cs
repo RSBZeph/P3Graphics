76760959bbabd506806b1e4990d7e5f42febf5cf
Jeroen Hijzelendoorn <j.m.hijzelendoorn@students.uu.nl>
2018-06-24 15:48:22 +0200

    sds

--- Code/SceneGraph.cs
+++ Code/SceneGraph.cs
@@ -17,7 +17,7 @@ class SceneGraph
     SoundPlayer music;
     
     Node root;
-    Node teapotN, floorN, minitN, empty, magikarpN;
+    Node teapotN, floorN, carN, empty, magikarpN;
     Texture wood, earth, cool;                    // texture to use for rendering
     Shader shader;                          // shader to use for rendering
     Shader postproc;                        // shader to use for post processing
@@ -96,11 +96,11 @@ class SceneGraph
         empty.children.Add(teapotN);
 
         floorN = new Node(shader, wood, floor);
-        floorN.localM = Matrix4.CreateTranslation(0, -2, 0) * Matrix4.Scale(4);
+        floorN.localM = Matrix4.Scale(4);
         empty.children.Add(floorN);
 
-        minitN = new Node(shader, cool, car);
-        empty.children.Add(minitN);
+        carN = new Node(shader, cool, car);
+        floorN.children.Add(carN);
 
         magikarpN = new Node(shader, cool, magikarp);
         magikarpN.localM = Matrix4.CreateTranslation(5, 0, 0);
@@ -130,10 +130,10 @@ class SceneGraph
         Matrix4 teapotT = Matrix4.CreateFromAxisAngle( new Vector3( 0, 1, 0 ), a);       
         teapotN.localM = teapotT;
         
-        Matrix4 minitT;
-        minitT = Matrix4.CreateTranslation(10, 0, 0);
-        minitT *= Matrix4.CreateFromAxisAngle( new Vector3( 0, 0, 1), a);
-        minitN.localM = minitT;
+        Matrix4 minitT = Matrix4.CreateFromAxisAngle( new Vector3( 0, 1, 0), 45);;
+        minitT = Matrix4.CreateTranslation(5, -2, 0);
+        minitT *= Matrix4.CreateFromAxisAngle( new Vector3( 0, 1, 0), 0);
+        carN.localM = minitT;
 
         Matrix4 magikarpT = Matrix4.CreateFromAxisAngle(new Vector3(1, 1, 1), a);
         magikarpT *= Matrix4.CreateTranslation(8, 4, 0);
